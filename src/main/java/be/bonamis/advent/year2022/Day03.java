7f9d26ff3a22468807b36d66eeabcb3145dd75d5
fbonamis <florent.bonamis@ppretail.com>
2022-12-03 20:51:05 +0100

    2022 Day 03 refactoring

--- src/main/java/be/bonamis/advent/year2022/Day03.java
+++ src/main/java/be/bonamis/advent/year2022/Day03.java
@@ -3,6 +3,8 @@ package be.bonamis.advent.year2022;
 import be.bonamis.advent.DaySolver;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day03 extends DaySolver<String> {
 
@@ -10,69 +12,48 @@ public class Day03 extends DaySolver<String> {
         super(puzzle);
     }
 
+    public static int alphabetPosition(char input) {
+        return Character.isUpperCase(input) ? input - 'A' + 27 : input - 'a' + 1;
+    }
+
     @Override
     public long solvePart01() {
-        return this.puzzle.stream().map(Day03::getResult).reduce(0L, Long::sum);
+        return this.puzzle.stream().map(line -> {
+            int half = line.length() / 2;
+            String part1 = line.substring(0, half);
+            String part2 = line.substring(half);
+            return alphabetPosition(getIntersection(List.of(charactersSet(part1), charactersSet(part2))).iterator().next());
+        }).reduce(0, Integer::sum);
     }
-
-    private static long getResult(String line) {
-        System.out.println("line: " + line);
-        int half = line.length() / 2;
-        String part1 = line.substring(0, half);
-        System.out.println("part1: " + part1);
-        String part2 = line.substring(half);
-        System.out.println("part2: " + part2);
-
-        return extracted(part1, part2);
-    }
-
-    private static int extracted(String part1, String part2) {
-        List<Character> list1 = getCharacters(part1);
-
-        List<Character> list2 = getCharacters(part2);
-
-        List<Character> collect = list1.stream()
-                .filter(list2::contains)
-                .toList();
-        return number(collect);
-    }
-
-    private static int number(List<Character> collect) {
-        Character common = collect.get(0);
-        String alphabet = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.println(common);
-
-        int index1 = Arrays.binarySearch(alphabet.toCharArray(), common) + 1;
-        int a = Character.toUpperCase(common) - 65 + 26 + 1;
-        return index1 == 0 ? a : index1;
-    }
-
 
     @Override
     public long solvePart02() {
-        int result = 0;
-        for (int i = 0; i < this.puzzle.size(); i += 3) {
-            int number = getNumber(this.puzzle, i + 2, i);
-            System.out.println("number1: " + number);
-            result += number;
-        }
-        return result;
+        return IntStream.range(0, this.puzzle.size())
+                .filter(idx -> idx % 3 == 0)
+                .map(i -> {
+                    Set<Character> intersection = getIntersection(List.of(
+                            charactersSet(this.puzzle.get(i)),
+                            charactersSet(this.puzzle.get(i + 1)),
+                            charactersSet(this.puzzle.get(i + 2))));
+                    return alphabetPosition(intersection.iterator().next());
+                })
+                .sum();
     }
 
-    private static int getNumber(List<String> lines, int halfSize, int start) {
-        List<Character> collect = getCharacters(lines.get(start));
-        for (int i = start + 1; i <= halfSize; i++) {
-            List<Character> list2 = getCharacters(lines.get(i));
-            System.out.println("count" + i);
-            collect = collect.stream()
-                    .filter(list2::contains)
-                    .toList();
-        }
-        return number(collect);
+    public static <T> Set<T> getIntersection(List<Set<T>> listOfSets) {
+        return listOfSets.stream().skip(1)
+                .collect(() -> new HashSet<>(listOfSets.get(0)), Set::retainAll, Set::retainAll);
     }
 
-    private static List<Character> getCharacters(String lines) {
-        return lines
+    public static Set<Character> charactersSet(String input) {
+        return input
+                .chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.toSet());
+    }
+
+    public static List<Character> charactersList(String input) {
+        return input
                 .chars()
                 .mapToObj(e -> (char) e)
                 .toList();
--- src/test/java/be/bonamis/advent/year2022/Day03Test.java
+++ src/test/java/be/bonamis/advent/year2022/Day03Test.java
@@ -1,8 +1,14 @@
 package be.bonamis.advent.year2022;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.*;
 
 import static be.bonamis.advent.utils.FileHelper.getLines;
+import static be.bonamis.advent.year2022.Day03.*;
+import static be.bonamis.advent.year2022.Day03.getIntersection;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day03Test {
@@ -17,5 +23,30 @@ class Day03Test {
     @Test
     void solvePart02() {
         assertThat(new Day03(getLines(CODE_TXT)).solvePart02()).isEqualTo(70);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"b,2", "c,3", "B,28", "C,29"})
+    void alphabetPosition_ReturnsThePositionOfTheLetterInTheAlphabet(char input, int expected) {
+        assertThat(alphabetPosition(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void intersection() {
+        Set<Integer> set01 = Set.of(1, 2, 3);
+        Set<Integer> set02 = Set.of(2, 3, 4);
+        Set<Integer> set03 = Set.of(3, 4, 5);
+        List<Set<Integer>> listOfSets = List.of(set01, set02, set03);
+        Set<Integer> intersection = getIntersection(listOfSets);
+        assertThat(intersection).containsExactly(3);
+    }
+
+    @Test
+    void commonItems() {
+        Set<Character> set01 = charactersSet("vJrwpWtwJgWr");
+        Set<Character> set02 = charactersSet("hcsFMMfFFhFp");
+        List<Set<Character>> listOfSets = List.of(set01, set02);
+        Set<Character> intersection = getIntersection(listOfSets);
+        assertThat(intersection).containsExactly('p');
     }
 }
