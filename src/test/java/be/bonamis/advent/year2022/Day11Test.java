08403196822c7fd274a3ccff1bff87096c69fa69
fbonamis <florent.bonamis@ppretail.com>
2022-12-11 09:48:27 +0100

    Day 11 2022 part 1 parsing OK not the logic yet

--- src/test/java/be/bonamis/advent/year2022/Day11Test.java
+++ src/test/java/be/bonamis/advent/year2022/Day11Test.java
@@ -1,10 +1,13 @@
 package be.bonamis.advent.year2022;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
+import org.jgrapht.alg.util.Pair;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static be.bonamis.advent.utils.FileHelper.getLines;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,10 +22,102 @@ class Day11Test {
         //log.info("Day10 part 01 result: {}", new ClockCircuit(lines).run());
 
     }
+
     @Test
     void solvePart01() {
         List<String> lines = getLines(CODE_TXT);
-        //new ClockCircuit(lines).run();
+        List<Monkey> monkeys = IntStream.range(0, (lines.size() + 1) / 7).mapToObj(i -> Monkey.of(lines, i * 7))
+                .toList();
+        System.out.println(monkeys);
+
+        for (Monkey monkey : monkeys) {
+            //Monkey monkey = monkeys.get(0);
+            List<Integer> startingItems = monkey.startingItems;
+            for (int i = 0; i < startingItems.size(); i++) {
+                Integer startingItem = startingItems.get(i);
+                Pair<Integer, Integer> boredAndReceiver = monkey.boredAndReceiver(startingItem);
+            /*int newLevel = monkey.executeOperation(startingItem);
+            int divisibleBy = monkey.test().divisibleBy();
+            int test = newLevel % divisibleBy;
+            int getBored = newLevel / 3;
+            System.out.println(getBored);
+            int monkeyReceiver;
+            if (test == 0) {
+                monkeyReceiver = monkey.ifTrue.monkeyReceiver;
+            } else {
+                monkeyReceiver = monkey.ifFalse.monkeyReceiver;
+            }*/
+                startingItems.remove(i);
+                monkeys.get(boredAndReceiver.getSecond()).add(boredAndReceiver.getFirst());
+            }
+        }
+        System.out.println(monkeys);
+        //}
+
+
         assertThat(lines.size()).isEqualTo(27);
+    }
+
+    record Monkey(List<Integer> startingItems, Operation operation, Test test, IfTrue ifTrue, IfFalse ifFalse) {
+
+        private static Monkey of(List<String> lines, int i) {
+            String[] items = data(lines.get(i + 1), "Starting items: ").replaceAll(" ", "").split(",");
+            List<Integer> startingItems = new ArrayList<>(Arrays.stream(items).map(Integer::parseInt).toList());
+
+            String[] operationData = data(lines.get(i + 2), "Operation: new = old ").split(" ");
+            Operation operation = new Operation(operationData[0], operationData[1]);
+
+            int test = Integer.parseInt(data(lines.get(i + 3), "Test: divisible by "));
+            int ifTrue = Integer.parseInt(data(lines.get(i + 4), "  If true: throw to monkey "));
+            int ifFalse = Integer.parseInt(data(lines.get(i + 5), "  If false: throw to monkey "));
+
+            return new Monkey(startingItems, operation, new Test(test), new IfTrue(ifTrue), new IfFalse(ifFalse));
+        }
+
+        private static String data(String input, String s) {
+            return input.substring(s.length() + 2);
+        }
+
+        public int executeOperation(Integer startingItem) {
+            int number = this.operation.number.equals("old") ? startingItem : Integer.parseInt(this.operation.number);
+            return switch (this.operation.operand) {
+                case "+" -> number + startingItem;
+                case "-" -> number - startingItem;
+                case "*" -> number * startingItem;
+                case "/" -> number / startingItem;
+                default -> throw new RuntimeException("");
+            };
+        }
+
+        Pair<Integer, Integer> boredAndReceiver(Integer startingItem) {
+            int newLevel = executeOperation(startingItem);
+            int divisibleBy = test().divisibleBy();
+            int test = newLevel % divisibleBy;
+            int getBored = newLevel / 3;
+            //System.out.println(getBored);
+            int monkeyReceiver;
+            if (test == 0) {
+                monkeyReceiver = ifTrue.monkeyReceiver;
+            } else {
+                monkeyReceiver = ifFalse.monkeyReceiver;
+            }
+            return new Pair<>(getBored, monkeyReceiver);
+        }
+
+        public void add(int getBored) {
+            this.startingItems.add(getBored);
+        }
+
+        record Operation(String operand, String number) {
+        }
+
+        record Test(int divisibleBy) {
+        }
+
+        record IfTrue(int monkeyReceiver) {
+        }
+
+        record IfFalse(int monkeyReceiver) {
+        }
     }
 }
