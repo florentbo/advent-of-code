011baee3174c61ed3fd9566426c8972d381d0650
fbonamis <florent.bonamis@ppretail.com>
2022-12-17 09:09:20 +0100

    Day 12 2022 part 2 OK using brute force

--- src/test/java/be/bonamis/advent/year2022/Day12Test.java
+++ src/test/java/be/bonamis/advent/year2022/Day12Test.java
@@ -3,17 +3,16 @@ package be.bonamis.advent.year2022;
 import be.bonamis.advent.common.CharGrid;
 import lombok.extern.slf4j.Slf4j;
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.*;
 
 import static be.bonamis.advent.utils.FileHelper.getLines;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,14 +24,46 @@ class Day12Test {
 
     public static void main(String[] args) {
         List<String> lines = getLines("2022/12/2022_12_input.txt");
-        log.info("Day11 part 01 result: {}", solvePart01(lines));
-        //log.info("Day11 part 02 result: {}", solvePart02(lines));
+        log.info("Day12 part 01 result: {}", solvePart01(lines));
+        log.info("Day12 part 02 result: {}", solvePart02(lines));
     }
 
     @Test
     void solvePart01() {
         List<String> lines = getLines(CODE_TXT);
         assertThat(solvePart01(lines)).isEqualTo(31);
+    }
+
+    private static long solvePart01(List<String> lines) {
+        CharGrid grid = new CharGrid(lines.stream().map(String::toCharArray).toArray(char[][]::new));
+        final var source = getPoint(grid, 'S');
+        final var sink = getPoint(grid, 'E');
+        return shortestPath(grid, source, sink);
+    }
+
+    @Test
+    void solvePart02() {
+        List<String> lines = getLines(CODE_TXT);
+        assertThat(solvePart02(lines)).isEqualTo(29);
+    }
+
+    private static long solvePart02(List<String> lines) {
+        CharGrid grid = new CharGrid(lines.stream().map(String::toCharArray).toArray(char[][]::new));
+        final var sink = getPoint(grid, 'E');
+        return filter(grid, 'a').map(point -> shortestPath(grid, point, sink)).min(Integer::compareTo).orElseThrow();
+    }
+
+    private static int shortestPath(CharGrid grid, Point source, Point sink) {
+        Graph<Point, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        grid.consume(graph::addVertex);
+        grid.consume(point -> addEdge(graph, point, grid));
+
+        DijkstraShortestPath<Point, DefaultEdge> shortestPath = new DijkstraShortestPath<>(graph);
+        GraphPath<Point, DefaultEdge> path = shortestPath.getPath(source, sink);
+        /*for (int i = 1; i < vertexList.size(); i++) {
+            System.out.println(grid.get(vertexList.get(i-1)) + " - " + grid.get(vertexList.get(i)));
+        }*/
+        return path!= null ? path.getVertexList().size() - 1: Integer.MAX_VALUE;
     }
 
     private static void addEdge(Graph<Point, DefaultEdge> graph, Point point, CharGrid grid) {
@@ -67,31 +98,12 @@ class Day12Test {
     }
 
 
-    @Test
-    @Disabled
-    void solvePart02() {
-        List<String> lines = getLines(CODE_TXT);
 
+    private static Point getPoint(CharGrid grid, char c) {
+        return filter(grid, c).findFirst().orElseThrow();
     }
 
-
-    private static long solvePart01(List<String> lines) {
-        CharGrid grid = new CharGrid(lines.stream().map(String::toCharArray).toArray(char[][]::new));
-        Graph<Point, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
-
-
-        grid.consume(graph::addVertex);
-        grid.consume(point -> addEdge(graph, point, grid));
-
-
-        final var source = grid.stream().filter(point -> grid.get(point) == 'S').findFirst().orElseThrow();
-        final var sink = grid.stream().filter(point -> grid.get(point) == 'E').findFirst().orElseThrow();
-
-        DijkstraShortestPath<Point, DefaultEdge> shortestPath = new DijkstraShortestPath<>(graph);
-        List<Point> vertexList = shortestPath.getPath(source, sink).getVertexList();
-        /*for (int i = 1; i < vertexList.size(); i++) {
-            System.out.println(grid.get(vertexList.get(i-1)) + " - " + grid.get(vertexList.get(i)));
-        }*/
-        return vertexList.size() - 1;
+    private static Stream<Point> filter(CharGrid grid, char c) {
+        return grid.stream().filter(point -> grid.get(point) == c);
     }
 }
