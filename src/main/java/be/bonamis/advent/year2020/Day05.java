749c8396980240b23e87adfee43c0a5a05f35718
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2023-12-01 18:41:00 +0100

    solving 2020 Day 05

--- src/main/java/be/bonamis/advent/year2020/Day05.java
+++ src/main/java/be/bonamis/advent/year2020/Day05.java
@@ -4,6 +4,7 @@ import be.bonamis.advent.DaySolver;
 import be.bonamis.advent.utils.FileHelper;
 import java.util.*;
 import lombok.extern.slf4j.Slf4j;
+import org.jgrapht.alg.util.Pair;
 
 @Slf4j
 public class Day05 extends DaySolver<String> {
@@ -12,9 +13,46 @@ public class Day05 extends DaySolver<String> {
     super(puzzle);
   }
 
+  public static long seatId(String line) {
+    String[] letters = line.split("");
+    Pair<Integer, Integer> moved = Pair.of(0, 127);
+    int latestLetterIndex = letters.length - 3;
+    for (int i = 0; i < latestLetterIndex; i++) {
+      log.debug("char: i {} val {}", i, letters[i]);
+      moved = move(letters[i], moved);
+      log.debug("new moved {}", moved);
+    }
+
+    int rowNumber = moved.getFirst();
+    log.debug("rowNumber {}", rowNumber);
+
+    Pair<Integer, Integer> column = Pair.of(0, 7);
+    for (int i = latestLetterIndex; i < letters.length; i++) {
+      log.debug("char: i {} val {}", i, letters[i]);
+      column = move(letters[i], column);
+      log.debug("new moved {}", column);
+    }
+    log.debug("column {}", column);
+    int colNumber = column.getFirst();
+    return rowNumber * 8L + colNumber;
+  }
+
+  public static Pair<Integer, Integer> move(String direction, Pair<Integer, Integer> rows) {
+    int i = rows.getSecond() - rows.getFirst();
+    if ("F".equals(direction) || "L".equals(direction)) {
+      return Pair.of(rows.getFirst(), i / 2 + rows.getFirst());
+    } else {
+      return Pair.of(roundUp(i, 2) + rows.getFirst(), rows.getSecond());
+    }
+  }
+
+  public static int roundUp(int num, int divisor) {
+    return (num + divisor - 1) / divisor;
+  }
+
   @Override
   public long solvePart01() {
-    return puzzle.size();
+    return this.puzzle.stream().map(Day05::seatId).max(Long::compare).orElseThrow();
   }
 
   @Override
@@ -24,7 +62,7 @@ public class Day05 extends DaySolver<String> {
   }
 
   public static void main(String[] args) {
-    String content = FileHelper.content("2023/01/2023_01_input.txt");
+    String content = FileHelper.content("2020/05/2020_05_input.txt");
     List<String> puzzle = Arrays.asList(content.split("\n"));
     Day05 day = new Day05(puzzle);
     log.info("solution part 1: {}", day.solvePart01());
--- src/test/java/be/bonamis/advent/year2020/Day05Test.java
+++ src/test/java/be/bonamis/advent/year2020/Day05Test.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -23,31 +25,20 @@ class Day05Test {
     "L,4,7,4,5",
   })
   void move(String direction, int start, int end, int expectedStart, int expectedEnd) {
-    assertThat(move(direction, Pair.of(start, end))).isEqualTo(Pair.of(expectedStart, expectedEnd));
+    assertThat(Day05.move(direction, Pair.of(start, end)))
+        .isEqualTo(Pair.of(expectedStart, expectedEnd));
   }
 
   @Test
   void line() {
-    String line = "FBFBBFFRLR";
-    String[] letters = line.split("");
-    Pair<Integer, Integer> moved;
-    for (int i = 0; i < letters.length - 3; i++) {
-      log.info("char: i {} val {}", i, letters[i]);
-      moved = move(letters[i], Pair.of(0, 127));
-      log.info("row: {}", moved.getSecond());
-    }
+    assertThat(Day05.seatId("FBFBBFFRLR")).isEqualTo(357);
+    assertThat(Day05.seatId("FFFBBBFRRR")).isEqualTo(119);
+    assertThat(Day05.seatId("BBFFBBFRLL")).isEqualTo(820);
   }
 
-  public static Pair<Integer, Integer> move(String direction, Pair<Integer, Integer> rows) {
-    int i = rows.getSecond() - rows.getFirst();
-    if ("F".equals(direction) || "L".equals(direction)) {
-      return Pair.of(rows.getFirst(), i / 2 + rows.getFirst());
-    } else {
-      return Pair.of(roundUp(i, 2) + rows.getFirst(), rows.getSecond());
-    }
-  }
-
-  public static int roundUp(int num, int divisor) {
-    return (num + divisor - 1) / divisor;
+  @Test
+  void solvePart01() {
+    assertThat(new Day05(List.of("FBFBBFFRLR", "FFFBBBFRRR", "BBFFBBFRLL")).solvePart01())
+        .isEqualTo(820);
   }
 }
