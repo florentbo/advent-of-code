2ca3580ea2057dab59269593bec13fb02ab71115
fbonamis <florent.bonamis@ppretail.com>
2023-12-08 09:26:55 +0100

    2023 Day 08 lcm attempt

--- src/main/java/be/bonamis/advent/year2023/Day08.java
+++ src/main/java/be/bonamis/advent/year2023/Day08.java
@@ -2,8 +2,9 @@ package be.bonamis.advent.year2023;
 
 import be.bonamis.advent.DaySolver;
 import be.bonamis.advent.utils.FileHelper;
+
+import java.math.BigInteger;
 import java.util.*;
-import java.util.ArrayList;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
@@ -72,16 +73,47 @@ public class Day08 extends DaySolver<String> {
 
   @Override
   public long solvePart02() {
-    Predicate<Node> arrivalPredicate = n -> n.arrival().endsWith("Z");
     List<Node> start = this.nodes.stream().filter(n -> n.arrival().endsWith("A")).toList();
-    List<Node> arrival = this.nodes.stream().filter(arrivalPredicate).toList();
 
-    log.debug("start: {}", start);
-    log.debug("arrival: {}", arrival);
-    return findPart2bis(start, arrival);
+    log.info("start: {}", start);
+    long part2bis = findPart2bis(start.get(0));
+    log.info("part2bis: {}", part2bis);
+    List<Long> list = start.stream().map(this::findPart2bis).toList();
+    log.info("list: {}", list);
+    int[] intArray = list.stream().mapToInt(Long::intValue).toArray();
+    return lcm(intArray);
+
+    // return findPart2bis(start);
   }
 
-  private int findPart2bis(List<Node> current, List<Node> arrival) {
+  int lcm(int[] input) {
+    int result = input[0];
+    for (int i = 1; i < input.length; i++) result = lcm(result, input[i]);
+    return result;
+  }
+
+  public int lcm(int number1, int number2) {
+    if (number1 == 0 || number2 == 0) {
+      return 0;
+    }
+    int absNumber1 = Math.abs(number1);
+    int absNumber2 = Math.abs(number2);
+    int absHigherNumber = Math.max(absNumber1, absNumber2);
+    int absLowerNumber = Math.min(absNumber1, absNumber2);
+    int lcm = absHigherNumber;
+    while (lcm % absLowerNumber != 0) {
+      lcm += absHigherNumber;
+    }
+    return lcm;
+  }
+
+  public BigInteger lcm(BigInteger number1, BigInteger number2) {
+    BigInteger gcd = number1.gcd(number2);
+    BigInteger absProduct = number1.multiply(number2).abs();
+    return absProduct.divide(gcd);
+  }
+
+  private int findPart2bis(List<Node> current) {
     int count = 0;
     int result = 0;
     while (result == 0) {
@@ -91,9 +123,30 @@ public class Day08 extends DaySolver<String> {
         log.debug("next: {}", next);
         if (next.stream().allMatch(n -> n.endsWith("Z"))) {
           result = count + 1;
-          log.debug("found: {} index: {}", arrival, result);
+          log.debug("found: {} index: {}", next, result);
         }
         List<Node> nextNode = next.stream().map(this::findNext).toList();
+        log.debug("nextNode: {}", nextNode);
+        current = nextNode;
+        count++;
+      }
+    }
+    return result;
+  }
+
+  private long findPart2bis(Node current) {
+    int count = 0;
+    int result = 0;
+    while (result == 0) {
+      for (String direction : this.leftRights) {
+        String next = direction.equals("R") ? current.right() : current.left();
+        log.debug("next: {}", next);
+        if (next.endsWith("Z")) {
+          result = count + 1;
+          log.debug("found: {} index: {}", next, result);
+        }
+        Node nextNode =
+            this.nodes.stream().filter(n -> n.arrival().equals(next)).findFirst().orElseThrow();
         log.debug("nextNode: {}", nextNode);
         current = nextNode;
         count++;
@@ -111,7 +164,7 @@ public class Day08 extends DaySolver<String> {
     List<String> puzzle = Arrays.asList(content.split("\n"));
     Day08 day = new Day08(puzzle);
     log.info("solution part 1: {}", day.solvePart01());
-    // log.info("solution part 2: {}", day.solvePart02());
+    log.info("solution part 2: {}", day.solvePart02());
   }
 
   public record Node(String arrival, String left, String right) {}
--- src/test/java/be/bonamis/advent/year2023/Day08Test.java
+++ src/test/java/be/bonamis/advent/year2023/Day08Test.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,7 +41,8 @@ ZZZ = (ZZZ, ZZZ)
 
   @Test
   void solvePart02() {
-    String text = """
+    String text =
+        """
 LR
 
 11A = (11B, XXX)
@@ -53,5 +56,23 @@ XXX = (XXX, XXX)
 """;
     day08 = new Day08(Arrays.asList(text.split("\n")));
     assertThat(day08.solvePart02()).isEqualTo(6);
+  }
+
+  Day08 day081 = new Day08(List.of(""));
+  @Test
+  void lcm() {
+
+    assertThat(day081.lcm(2, 3)).isEqualTo(6);
+    List<Integer> list = List.of(3, 4, 6);
+    int[] array = list.stream().mapToInt(i -> i).toArray();
+    assertThat(day081.lcm(array)).isEqualTo(12);
+  }
+
+  @Test
+  public void testLCM() {
+    BigInteger number1 = new BigInteger("12");
+    BigInteger number2 = new BigInteger("18");
+    BigInteger expectedLCM = new BigInteger("36");
+    assertThat(day081.lcm(number1,number2)).isEqualTo(expectedLCM);
   }
 }
