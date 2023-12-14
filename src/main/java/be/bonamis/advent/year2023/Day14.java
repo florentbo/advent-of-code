bc6aaa5653d1d01c2f4bfda482058210b28780ca
fbonamis <florent.bonamis@ppretail.com>
2023-12-14 19:02:47 +0100

    2023 Day 14 part 2 ok and cleaning too

--- src/main/java/be/bonamis/advent/year2023/Day14.java
+++ src/main/java/be/bonamis/advent/year2023/Day14.java
@@ -4,17 +4,26 @@ import be.bonamis.advent.DaySolver;
 import be.bonamis.advent.common.CharGrid;
 import be.bonamis.advent.utils.FileHelper;
 import java.awt.*;
-import java.util.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.*;
+import java.util.stream.Stream;
 
+import be.bonamis.advent.utils.marsrover.Position;
+import be.bonamis.advent.utils.marsrover.Rover;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+
+import static be.bonamis.advent.utils.marsrover.Rover.Direction.*;
+import static be.bonamis.advent.utils.marsrover.Rover.Direction.EAST;
 
 @Slf4j
 @Getter
 public class Day14 extends DaySolver<String> {
+
+  private static final char ROCK = 'O';
+  private static final char DOT = '.';
 
   private final CharGrid grid;
 
@@ -25,57 +34,116 @@ public class Day14 extends DaySolver<String> {
 
   @Override
   public long solvePart01() {
-    List<List<Point>> columns = grid.columns();
-    return columns.stream().flatMap(col -> rocks(col).stream()).reduce(Long::sum).orElseThrow();
-  }
-
-  List<Long> rocks(List<Point> column) {
-    List<String> values =
-        column.stream()
-            .map(grid::get)
-            .map(Object::toString)
-            .toList(); // Convert each character to its string representation
-
-    log.debug("values: {}", values);
-    Map<Integer, String> map = listToMap(values);
-    log.debug("map: {}", map);
-    long rockHeight = column.size() + 1L;
-    List<Long> rocks = new ArrayList<>();
-    for (Map.Entry<Integer, String> mapEntry : map.entrySet()) {
-      log.debug("mapEntry: {}", mapEntry);
-      int key = mapEntry.getKey();
-      String value = mapEntry.getValue();
-      log.debug("rockHeight: {}", rockHeight);
-      if (value.equals("O")) {
-        rockHeight--;
-        rocks.add(rockHeight);
-      }
-      if (value.equals("#")) {
-        rockHeight = key;
-      }
-    }
-    log.debug("rocks: {}", rocks);
-    return rocks;
-  }
-
-  Map<Integer, String> listToMap(List<String> originalArray) {
-    TreeMap<Integer, String> map = new TreeMap<>(Collections.reverseOrder());
-
-    int position = originalArray.size();
-
-    for (String character : originalArray) {
-      if (!character.equals(".")) {
-        map.put(position, character);
-      }
-      position--;
-    }
-
-    return map;
+    moveRocks(grid, SOUTH);
+    return loadsSum(new CharGrid(grid.rowsAsLines()));
   }
 
   @Override
   public long solvePart02() {
-    return 0;
+    return solve(grid);
+  }
+
+  private Integer solve(CharGrid grid) {
+    Map<List<String>, Pair<Integer, Integer>> memo = memo(grid);
+    log.debug("memo size {}", memo.size());
+    List<Pair<Integer, Integer>> values = new ArrayList<>(memo.values());
+    log.debug("unsorted values {}", values);
+    List<Integer> memoValues = values.stream().map(Pair::getLeft).toList();
+    log.debug("memo: {}", memoValues);
+    Pair<Integer, Integer> series = findSeries(memoValues);
+    log.debug("series {}", series);
+
+    Integer seriesSize = series.getRight();
+    Integer seriesPrefix = series.getLeft();
+    int modulo = (1000000000 - seriesPrefix) % seriesSize;
+    log.debug("modulo {}", modulo);
+
+    return values.get(seriesPrefix + modulo - 1).getRight();
+  }
+
+  Map<List<String>, Pair<Integer, Integer>> memo(CharGrid grid) {
+    Map<List<String>, Pair<Integer, Integer>> memo = new LinkedHashMap<>();
+
+    for (int i = 1; i <= 500; i++) {
+      cycle(grid);
+      List<String> lines = grid.rowsAsLines();
+      Integer loadsSum = loadsSum(new CharGrid(lines));
+      memo.put(lines, Pair.of(i, loadsSum));
+      // log.debug("sum: {} value {}", loadsSum, i);
+    }
+    return memo;
+  }
+
+  Integer loadsSum(CharGrid grid) {
+    return loads(grid).stream().reduce(Integer::sum).orElseThrow();
+  }
+
+  private List<Integer> loads(CharGrid grid) {
+    return grid.stream()
+        .filter(p1 -> grid.get(p1).equals('O'))
+        .map(p -> grid.getHeight() - p.y)
+        .toList();
+  }
+
+  void cycle(CharGrid grid) {
+    moveRocks(grid, SOUTH);
+    moveRocks(grid, WEST);
+    moveRocks(grid, NORTH);
+    moveRocks(grid, EAST);
+  }
+
+  Pair<Integer, Integer> findSeries(List<Integer> list) {
+    int size = list.size();
+    OptionalInt first =
+        IntStream.range(0, size)
+            .map(i -> list.size() - i - 1)
+            .filter(i -> list.get(i) - list.get(i - 1) > 1)
+            .findFirst();
+    if (first.isPresent()) {
+      int index = first.getAsInt();
+      return Pair.of(index, size - index);
+    }
+    return Pair.of(0, 0);
+  }
+
+  void moveRocks(CharGrid grid, Rover.Direction direction) {
+    Stream<Point> stream = stream(grid, direction);
+    stream.forEach(
+        point -> {
+          Character c = grid.get(point);
+          // log.debug("point {} value {}", point, c);
+          if (c.equals(ROCK)) {
+            Rover rover = new Rover(direction, new Position(point.x, point.y));
+            boolean canMove = true;
+            while (canMove) {
+              Position newPosition = rover.move(Rover.Command.FORWARD).position();
+              Point movedPoint = new Point(newPosition.x(), newPosition.y());
+              canMove = isInTheGrid(grid, movedPoint) && isDot(grid, movedPoint);
+              if (canMove) {
+                grid.set(movedPoint, ROCK);
+                Position position = rover.position();
+                grid.set(new Point(position.x(), position.y()), DOT);
+                rover = rover.move(Rover.Command.FORWARD);
+              }
+            }
+          }
+        });
+  }
+
+  private Stream<Point> stream(CharGrid grid, Rover.Direction direction) {
+    return switch (direction) {
+      case NORTH -> grid.streamFromDown();
+      case EAST -> grid.streamFromLeft();
+      default -> grid.stream();
+    };
+  }
+
+  private boolean isDot(CharGrid grid, Point movedPoint) {
+    return grid.get(movedPoint).equals(DOT);
+  }
+
+  private boolean isInTheGrid(CharGrid grid, Point movedPoint) {
+    return grid.isInTheGrid().test(movedPoint);
   }
 
   public static void main(String[] args) {
--- src/test/java/be/bonamis/advent/year2023/Day14Test.java
+++ src/test/java/be/bonamis/advent/year2023/Day14Test.java
@@ -1,29 +1,20 @@
 package be.bonamis.advent.year2023;
 
 import be.bonamis.advent.common.CharGrid;
-import be.bonamis.advent.utils.FileHelper;
-import be.bonamis.advent.utils.marsrover.Position;
-import be.bonamis.advent.utils.marsrover.Rover;
-import be.bonamis.advent.utils.marsrover.Rover.Direction;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
-import static be.bonamis.advent.utils.marsrover.Rover.Direction.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class Day14Test {
 
-  private static final char ROCK = 'O';
-  private static final char DOT = '.';
   private final List<String> input =
       Arrays.asList(
           """
@@ -46,41 +37,9 @@ class Day14Test {
   }
 
   @Test
-  void rocks() {
-    List<List<Point>> columns = day14.getGrid().columns();
-    assertThat(day14.rocks(columns.get(2))).isEqualTo(List.of(10L, 4L, 3L));
-  }
-
-  @Test
-  void rocks2() {
-    String content = FileHelper.content("2023/14/2023_14_input.txt");
-    List<String> puzzle = Arrays.asList(content.split("\n"));
-    Day14 day = new Day14(puzzle);
-    List<List<Point>> columns = day.getGrid().columns();
-    assertThat(day.rocks(columns.get(0))).contains(100L, 99L, 91L);
-  }
-
-  @Test
   void moveRocks() {
-    CharGrid grid = new CharGrid(input);
-    int height = grid.getHeight();
-    log.debug("before moved");
-    grid.printLines();
-
-    moveRocks(grid, SOUTH);
-    Integer sum = loadsSum(grid);
+    long sum = day14.solvePart01();
     assertThat(sum).isEqualTo(136);
-  }
-
-  private Integer loadsSum(CharGrid grid) {
-    return loads(grid).stream().reduce(Integer::sum).orElseThrow();
-  }
-
-  private List<Integer> loads(CharGrid grid) {
-    return grid.stream()
-        .filter(p1 -> grid.get(p1).equals('O'))
-        .map(p -> grid.getHeight() - p.y)
-        .toList();
   }
 
   @Test
@@ -104,7 +63,7 @@ class Day14Test {
     grid.printLines();
 
     for (int i = 0; i < 3; i++) {
-      cycle(grid);
+      day14.cycle(grid);
     }
     log.debug("\n\nAfter cycle 03");
     log.info("rows: {}", grid.rowsAsLines());
@@ -113,103 +72,8 @@ class Day14Test {
 
   @Test
   void solvePart2() {
-    CharGrid grid = new CharGrid(input);
-    log.debug("before moved");
-    grid.printLines();
-
-    Map<List<String>, Integer> memo = getMemo(grid);
-    int modulo = 1000000000 % memo.size();
-    log.debug("memo {}", memo.size());
-    log.debug("modulo {}", modulo);
-
-    /*for (Map.Entry<List<String>, Integer> entry : memo.entrySet()) {
-      List<String> key = entry.getKey();
-      CharGrid charGrid = new CharGrid(key);
-      Integer sum = loadsSum(charGrid);
-      log.debug("sum: {}", sum);
-    }*/
-    log.debug("memo: {}", memo.values());
-
-    int index = 25 + modulo + 1;
-
-    log.debug("index: {}", index);
-
-    List<String> key =
-        memo.entrySet().stream()
-            .filter(entry -> entry.getValue().equals(index))
-            .findFirst()
-            .orElseThrow()
-            .getKey();
-
-    assertThat(loadsSum(new CharGrid(key))).isEqualTo(64);
-  }
-
-  private Map<List<String>, Integer> getMemo(CharGrid grid) {
-    Map<List<String>, Integer> memo = new HashMap<>();
-
-    /*for (int i = 1; i <= 10; i++) {
-      cycle(grid);
-    }*/
-    for (int i = 1; i <= 30; i++) {
-      cycle(grid);
-      List<String> lines = grid.rowsAsLines();
-      /*if (memo.containsKey(lines)) {
-        return memo;
-      }*/
-      memo.put(lines, i);
-      CharGrid charGrid = new CharGrid(lines);
-      Integer sum = loadsSum(charGrid);
-      log.debug("sum: {} value {}", sum, i);
-
-    }
-    return memo;
-  }
-
-  private void cycle(CharGrid grid) {
-    moveRocks(grid, SOUTH);
-    moveRocks(grid, WEST);
-    moveRocks(grid, NORTH);
-    moveRocks(grid, EAST);
-  }
-
-  private void moveRocks(CharGrid grid, Direction direction) {
-    Stream<Point> stream = stream(grid, direction);
-    stream.forEach(
-        point -> {
-          Character c = grid.get(point);
-          // log.debug("point {} value {}", point, c);
-          if (c.equals(ROCK)) {
-            Rover rover = new Rover(direction, new Position(point.x, point.y));
-            boolean canMove = true;
-            while (canMove) {
-              Position newPosition = rover.move(Rover.Command.FORWARD).position();
-              Point movedPoint = new Point(newPosition.x(), newPosition.y());
-              canMove = isInTheGrid(grid, movedPoint) && isDot(grid, movedPoint);
-              if (canMove) {
-                grid.set(movedPoint, ROCK);
-                Position position = rover.position();
-                grid.set(new Point(position.x(), position.y()), DOT);
-                rover = rover.move(Rover.Command.FORWARD);
-              }
-            }
-          }
-        });
-  }
-
-  private Stream<Point> stream(CharGrid grid, Direction direction) {
-    return switch (direction) {
-      case NORTH -> grid.streamFromDown();
-      case EAST -> grid.streamFromLeft();
-      default -> grid.stream();
-    };
-  }
-
-  private boolean isDot(CharGrid grid, Point movedPoint) {
-    return grid.get(movedPoint).equals(DOT);
-  }
-
-  private boolean isInTheGrid(CharGrid grid, Point movedPoint) {
-    return grid.isInTheGrid().test(movedPoint);
+    long result = day14.solvePart02();
+    assertThat(result).isEqualTo(64);
   }
 
   @Test
@@ -238,6 +102,13 @@ class Day14Test {
     assertThat(array(grid.stream(), grid)).isEqualTo("0123");
     assertThat(array(grid.streamFromDown(), grid)).isEqualTo("1032");
     assertThat(array(grid.streamFromLeft(), grid)).isEqualTo("2301");
+  }
+
+  @Test
+  void findSeries() {
+    List<Integer> list = List.of(1, 2, 11, 12, 13, 14, 15, 16, 17);
+
+    assertThat(day14.findSeries(list)).isEqualTo(Pair.of(2, 7));
   }
 
   private String array(Stream<Point> points, CharGrid grid) {
