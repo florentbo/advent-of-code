648ad715a18e409da97234096678a4cbdc3f1f5c
fbonamis <florent.bonamis@ppretail.com>
2023-12-16 23:16:31 +0100

    2023 Day 16 part 1 OK

--- src/main/java/be/bonamis/advent/year2023/Day16.java
+++ src/main/java/be/bonamis/advent/year2023/Day16.java
@@ -8,7 +8,9 @@ import be.bonamis.advent.utils.marsrover.Rover;
 import java.awt.*;
 import java.util.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import be.bonamis.advent.utils.marsrover.Rover.Command;
 import lombok.Getter;
@@ -29,6 +31,8 @@ public class Day16 extends DaySolver<String> {
 
   private final CharGrid grid;
 
+
+
   public Day16(List<String> puzzle) {
     super(puzzle);
     this.grid = new CharGrid(puzzle);
@@ -38,88 +42,89 @@ public class Day16 extends DaySolver<String> {
   public long solvePart01() {
     // grid.stream().skip(8).limit(15).forEach(x -> log.debug("point {} value {}", x ,
     // grid.get(x)));
+    final Set<Rover> energized = new HashSet<>();
     Position position = new Position(-1, 0);
     Rover rover = new Rover(EAST, position);
-    Beam beam = Beam.of(rover);
-    List<Beam> beams = new ArrayList<>(List.of(beam));
-    move(beams);
+    // Beam beam = Beam.of(rover);
+    // List<Beam> beams = new ArrayList<>(List.of(beam));
+    move(List.of(rover), energized);
 
-    return this.puzzle.size();
+    log.debug("energized size {}", energized.size());
+    Set<Position> positions = energized.stream().map(Rover::position).collect(Collectors.toSet());
+    return positions.size();
   }
 
-  private void move(List<Beam> beams) {
-    beams.forEach(this::move);
+  private void move(List<Rover> beams, Set<Rover> energized) {
+    beams.forEach(rover -> move(rover, energized));
   }
 
-  private void move(Beam beam) {
-    Rover rover = beam.rover();
+  /* private void move(Rover beam) {
+    Rover rover = beam;
     log.debug("inside single bean move rover {} data ", rover);
     move(rover);
-  }
+  }*/
 
-  record Beam(Rover rover, List<Position> positions) {
-    public static Beam of(Rover rover) {
-      return new Beam(rover, new ArrayList<>(List.of(rover.position())));
-    }
-  }
-
-  private void move(Rover rover) {
+  private void move(Rover rover, Set<Rover> energized) {
     log.debug("inside move data  rover {}", rover);
     Rover moved = moveRover(rover, FORWARD);
     if (isInTheGrid(moved)) {
       char data = data(moved);
       log.debug("adding point at {} with data {}", moved.position(), data);
+      energized.add(moved);
 
       switch (data) {
         case DOT -> {
           log.debug("dot point at {} with data {}", moved.position(), data);
-          move(moved);
+          move(moved,energized);
         }
         case VERTICAL_SPLITTER -> {
           log.debug("vert split point ");
           if (List.of(EAST, WEST).contains(moved.direction())) {
-            facialSplit(moved);
+            facialSplit(moved, energized);
           } else {
-            move(moved);
+            move(moved, energized);
           }
         }
         case HORIZONTAL_SPLITTER -> {
           log.debug("horiz split point ");
           if (List.of(SOUTH, NORTH).contains(moved.direction())) {
-            facialSplit(moved);
+            facialSplit(moved, energized);
           } else {
-            move(moved);
+            move(moved, energized);
           }
         }
         case RIGHT_MIRROR -> {
           log.debug("right mirror point ");
           switch (moved.direction()) {
-            case WEST, EAST -> move(moveRover(moved, LEFT));
-            case NORTH, SOUTH -> move(moveRover(moved, RIGHT));
+            case WEST, EAST -> move(moveRover(moved, LEFT), energized);
+            case NORTH, SOUTH -> move(moveRover(moved, RIGHT), energized);
           }
         }
         case LEFT_MIRROR -> {
           log.debug("left mirror point ");
           switch (moved.direction()) {
-            case WEST, EAST -> move(moveRover(moved, RIGHT));
-            case NORTH, SOUTH -> move(moveRover(moved, LEFT));
+            case WEST, EAST -> move(moveRover(moved, RIGHT), energized);
+            case NORTH, SOUTH -> move(moveRover(moved, LEFT), energized);
           }
         }
       }
     }
   }
 
-  private void facialSplit(Rover rover) {
-    List<Beam> beams = new ArrayList<>();
-    addBeam(moveRover(rover, LEFT), beams, LEFT);
-    addBeam(moveRover(rover, RIGHT), beams, RIGHT);
-    move(beams);
+  private void facialSplit(Rover rover, Set<Rover> energized) {
+    List<Rover> beams = new ArrayList<>();
+    addBeam(moveRover(rover, LEFT), beams, LEFT, energized);
+    addBeam(moveRover(rover, RIGHT), beams, RIGHT, energized);
+    move(beams, energized);
   }
 
-  private void addBeam(Rover rover, List<Beam> beams, Command direction) {
+  private void addBeam(Rover rover, List<Rover> beams, Command direction, Set<Rover> energized) {
     log.debug("\n\nmoved " + direction);
     logRover(rover, direction);
-    beams.add(Beam.of(rover));
+    if (!energized.contains(rover)) {
+      energized.add(rover);
+      beams.add(rover);
+    }
   }
 
   private boolean isInTheGrid(Rover rover) {
@@ -149,7 +154,7 @@ public class Day16 extends DaySolver<String> {
   }
 
   public static void main(String[] args) {
-    String content = FileHelper.content("2023/14/2023_14_input.txt");
+    String content = FileHelper.content("2023/16/2023_16_input.txt");
     List<String> puzzle = Arrays.asList(content.split("\n"));
     Day16 day = new Day16(puzzle);
     log.info("solution part 1: {}", day.solvePart01());
