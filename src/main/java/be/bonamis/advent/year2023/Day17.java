62dfa446d43a58249e4243bf2ab8e7989f5acf1f
fbonamis <florent.bonamis@ppretail.com>
2023-12-27 14:34:11 +0100

    2023 day 17 part 1 not very far

--- src/main/java/be/bonamis/advent/year2023/Day17.java
+++ src/main/java/be/bonamis/advent/year2023/Day17.java
@@ -2,26 +2,16 @@ package be.bonamis.advent.year2023;
 
 import be.bonamis.advent.DaySolver;
 import be.bonamis.advent.common.CharGrid;
-import be.bonamis.advent.common.Grid;
 import be.bonamis.advent.utils.FileHelper;
 
-import java.awt.*;
 import java.util.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
-import be.bonamis.advent.utils.marsrover.Position;
-import be.bonamis.advent.utils.marsrover.Rover;
+import be.bonamis.advent.utils.marsrover.*;
 import be.bonamis.advent.year2023.poc.DijkstraAlgorithm;
 import be.bonamis.advent.year2023.poc.DijkstraAlgorithm.Result;
 import be.bonamis.advent.year2023.poc.Node;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.jgrapht.alg.interfaces.*;
-import org.jgrapht.alg.shortestpath.*;
-import org.jgrapht.graph.*;
 
 import static be.bonamis.advent.utils.marsrover.Rover.Command.FORWARD;
 import static be.bonamis.advent.utils.marsrover.Rover.Direction.values;
@@ -31,69 +21,15 @@ import static java.util.stream.Collectors.*;
 @Getter
 public class Day17 extends DaySolver<String> {
 
-  private final int[][] data;
-  private final Grid grid;
   private final CharGrid charGrid;
 
   public Day17(List<String> puzzle) {
     super(puzzle);
-    this.data =
-        puzzle.parallelStream()
-            .map(l -> l.trim().split(""))
-            .map(sa -> Stream.of(sa).mapToInt(Integer::parseInt).toArray())
-            .toArray(int[][]::new);
-    grid = new Grid(data);
     charGrid = new CharGrid(puzzle);
   }
 
   @Override
   public long solvePart01() {
-    return shortestPath(grid);
-  }
-
-  private long shortestPath(Grid grid) {
-    var graph =
-        new DefaultDirectedWeightedGraph<Point, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-    grid.consume(graph::addVertex);
-    grid.consume(point -> addEdge(graph, point, grid));
-
-    ShortestPathAlgorithm<Point, DefaultWeightedEdge> dijkstraShortestPath =
-        new DijkstraShortestPath<>(graph);
-
-    final var source = new Point(0, 0);
-    final var sink = new Point(grid.getHeight() - 1, grid.getWidth() - 1);
-    return (long) dijkstraShortestPath.getPathWeight(source, sink);
-  }
-
-  private void addEdge(
-      DefaultDirectedWeightedGraph<Point, DefaultWeightedEdge> graph, Point point, Grid grid) {
-    var value = grid.get(point);
-    for (var adjacent : adjacentPoints(point, grid)) {
-      var edge = graph.addEdge(adjacent, point);
-      graph.setEdgeWeight(edge, value);
-    }
-  }
-
-  private Collection<Point> adjacentPoints(Point point, Grid grid) {
-    var points = new HashSet<Point>();
-
-    addPoint(points, point.x, point.y - 1, grid);
-    addPoint(points, point.x, point.y + 1, grid);
-    addPoint(points, point.x - 1, point.y, grid);
-    addPoint(points, point.x + 1, point.y, grid);
-
-    return points;
-  }
-
-  private void addPoint(HashSet<Point> points, int x, int y, Grid grid) {
-    final var point = new Point(x, y);
-    Integer value = grid.get(point);
-    if (value != null) {
-      points.add(point);
-    }
-  }
-
-  public long solveCustomShortest() {
     Map<Position, Node<Position>> nodes = new HashMap<>();
     charGrid.consume(
         point -> {
@@ -103,51 +39,37 @@ public class Day17 extends DaySolver<String> {
         });
 
     Position startPosition = Position.of(0, 0);
-    log.info("startPosition {}", startPosition);
-    Position endPosition = Position.of(grid.getWidth() - 1, grid.getHeight() - 1);
-    log.info("endPosition {}", endPosition);
+    log.info("startPosition {} value {} ", startPosition, value(startPosition));
+
+    Position endPosition = Position.of(charGrid.getWidth() - 1, charGrid.getHeight() - 1);
+    log.info("endPosition {} value {} ", endPosition, value(endPosition));
     Node<Position> sourceNode = nodes.get(startPosition);
     Node<Position> sinkNode = nodes.get(endPosition);
 
     Result<Position> result =
         new PositionDijkstraAlgorithm(charGrid, nodes)
             .calculateShortestPathFromSource(sourceNode, sinkNode, false);
+    //result.path().forEach(p -> log.debug("pos {} value {}", p, value(p.getValue())));
+    Optional<Integer> sum =
+        result.path().stream().map(node -> value(node.getValue())).reduce(Integer::sum);
+    sum.ifPresent(
+        x -> {
+          System.out.println(x);
+          log.info("sum distance {}", x);
+        });
 
-    return result.distance();
+    int distance = result.distance();
+    log.info("result distance {}", distance);
+
+    int end = value(endPosition);
+    int start = value(startPosition);
+    log.info("start {} end {}", start, end);
+    return distance + end;
   }
 
-  public long solveShortestPathDijkstra(boolean extraValidation) {
-    // Graph<Point> graph = new Graph<>();
-    Map<Point, Node<Point>> nodes = new HashMap<>();
-    grid.consume(
-        point -> {
-          Node<Point> node = new Node<>(point);
-          nodes.put(point, node);
-          // graph.addNode(node);
-        });
-    grid.consume(
-        point -> {
-          Integer value = grid.get(point);
-          Node<Point> node = nodes.get(point);
-          for (var adjacent : adjacentPoints(point, grid)) {
-            Node<Point> adjacentNode = nodes.get(adjacent);
-            node.addDestination(adjacentNode, value);
-            // log.debug("Adding edge {} to {}", value, adjacentNode);
-            // log.debug("node {} adj {}", adjacentNode, adjacentNode.getAdjacentNodes());
-            // log.debug("node adja");
-          }
-        });
-
-    final var source = new Point(0, 0);
-    final var sink = new Point(grid.getHeight() - 1, grid.getWidth() - 1);
-    Node<Point> sourceNode = nodes.get(source);
-    Node<Point> sinkNode = nodes.get(sink);
-    Result<Point> result =
-        new PointDijkstraAlgorithm()
-            .calculateShortestPathFromSource(sourceNode, sinkNode, extraValidation);
-
-    result.path().forEach(n -> log.debug("{}", n.getValue()));
-    return result.distance();
+  private int value(Position node) {
+    char data = charGrid.get(node);
+    return Character.getNumericValue(data);
   }
 
   @Override
@@ -162,8 +84,6 @@ public class Day17 extends DaySolver<String> {
     log.info("solution part 1: {}", day.solvePart01());
     log.info("solution part 2: {}", day.solvePart02());
   }
-
-  static class PointDijkstraAlgorithm extends DijkstraAlgorithm<Point> {}
 
   static class PositionDijkstraAlgorithm extends DijkstraAlgorithm<Position> {
     private final CharGrid charGrid;
@@ -195,26 +115,23 @@ public class Day17 extends DaySolver<String> {
                 return rover.move(FORWARD, true);
               })
           .filter(charGrid::isPositionInTheGrid)
-          // .filter(this::isNotForest)
-          .filter(rover -> canContinueInTheSameDirection(rover, currentShortestPath))
           .map(Rover::position)
+          .filter(position -> canContinueInTheSameDirection(currentShortestPath, position))
           .collect(toSet());
     }
 
     private boolean canContinueInTheSameDirection(
-        Rover rover, List<Node<Position>> currentShortestPath) {
-      Position position = rover.position();
+        List<Node<Position>> currentShortestPath, Position position) {
       List<Position> positions = currentShortestPath.stream().map(Node::getValue).toList();
-      log.debug("positions {}", positions);
+      // log.debug("positions {}", positions);
       if (positions.size() > 3) {
         List<Position> latest3AndActual =
             new ArrayList<>(positions.subList(positions.size() - 3, positions.size()));
         latest3AndActual.add(position);
-        log.debug("latest3 {}", latest3AndActual);
+        // log.debug("latest3 {}", latest3AndActual);
         Set<Long> xValues = latest3AndActual.stream().map(Position::x).collect(toSet());
         Set<Long> yValues = latest3AndActual.stream().map(Position::y).collect(toSet());
         boolean allTheSame = xValues.size() == 1 || yValues.size() == 1;
-        log.debug("allTheSame {}", allTheSame);
         return !allTheSame;
       }
       return true;
--- src/test/java/be/bonamis/advent/year2023/Day17Test.java
+++ src/test/java/be/bonamis/advent/year2023/Day17Test.java
@@ -4,44 +4,17 @@ import static org.assertj.core.api.Assertions.*;
 
 import be.bonamis.advent.utils.FileHelper;
 import java.util.*;
+
 import lombok.extern.slf4j.*;
 import org.junit.jupiter.api.*;
 
 @Slf4j
 class Day17Test {
 
-  private Day17 day17;
-
-  @BeforeEach
-  void setUp() {}
-
   @Test
-  void solvePart01_2021Day15() {
-    // String content = FileHelper.content("2023/17/2023_17_00_code.txt");
-    String content = FileHelper.content("2023/17/2021_day15_test.txt");
-    day17 = new Day17(Arrays.asList(content.split("\n")));
-    assertThat(day17.solvePart01()).isEqualTo(40);
-  }
-
-  @Test
-  void solvePart01_2021Day15CustomAlgo() {
-    String content = FileHelper.content("2023/17/2021_day15_test.txt");
-    day17 = new Day17(Arrays.asList(content.split("\n")));
-    assertThat(day17.solveShortestPathDijkstra(false)).isEqualTo(40);
-  }
-
-  @Test@Disabled("not working yet")
   void solvePart01() {
     String content = FileHelper.content("2023/17/2023_17_00_code.txt");
-    day17 = new Day17(Arrays.asList(content.split("\n")));
-    assertThat(day17.solveShortestPathDijkstra(true)).isEqualTo(102);
-  }
-
-  @Test
-  @Disabled("infinite loop")
-  void solvePart01Custom() {
-    String content = FileHelper.content("2023/17/2023_17_00_code.txt");
-    day17 = new Day17(Arrays.asList(content.split("\n")));
-    assertThat(day17.solveCustomShortest()).isEqualTo(102);
+    Day17 day17 = new Day17(Arrays.asList(content.split("\n")));
+    assertThat(day17.solvePart01()).isEqualTo(102);
   }
 }
