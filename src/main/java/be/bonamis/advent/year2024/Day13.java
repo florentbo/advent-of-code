cd7245bd031f79610df695ab96d51191cbf4e192
fbonamis <florent.bonamis@ppretail.com>
2024-12-21 23:00:50 +0100

    2024 day 13 sample ok

---
+++ src/main/java/be/bonamis/advent/year2024/Day13.java
@@ -0,0 +1,138 @@
+package be.bonamis.advent.year2024;
+
+import be.bonamis.advent.TextDaySolver;
+import java.io.InputStream;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class Day13 extends TextDaySolver {
+  private final Inputs inputs;
+
+  public Day13(InputStream inputStream) {
+    super(inputStream);
+    this.inputs = Inputs.of(this.puzzle);
+  }
+
+  public Day13(String input) {
+    super(input);
+    this.inputs = Inputs.of(this.puzzle);
+  }
+
+  @Override
+  public long solvePart01() {
+    return this.inputs.inputs().stream()
+        .map(Input::intersectionPoint)
+        .map(point -> point.x() * 3 + point.y())
+        .reduce(0L, Long::sum);
+  }
+
+  @Override
+  public long solvePart02() {
+    return 0;
+  }
+
+  public record Inputs(List<Input> inputs) {
+    public static Inputs of(List<String> puzzle) {
+      List<Input> list =
+          IntStream.range(0, puzzle.size() / 4)
+              .mapToObj(
+                  i -> Input.of(puzzle.get(i * 4), puzzle.get(i * 4 + 1), puzzle.get(i * 4 + 2)))
+              .toList();
+      return new Inputs(list);
+    }
+  }
+
+  record Input(Button buttonA, Button buttonB, Prize prize) {
+    public static Input of(Button buttonA, Button buttonB, Prize prize) {
+      return new Input(buttonA, buttonB, prize);
+    }
+
+    public static Input of(String s, String s1, String s2) {
+      return Input.of(Button.of(s), Button.of(s1), Prize.of(s2));
+    }
+
+    LineEquation.Point intersectionPoint() {
+      LineEquation line1 = LineEquation.of(buttonA.x(), buttonB().x(), -prize.x());
+      LineEquation line2 = LineEquation.of(buttonA.y(), buttonB().y(), -prize.y());
+      LineEquation.Point point = line1.intersectionPoint(line2);
+      return point.x() > 100 || point.y() > 100 ? LineEquation.Point.of(0, 0) : point;
+    }
+  }
+
+  record LineEquation(double x, double y, double c) {
+    public static LineEquation of(double x, double y, double c) {
+      return new LineEquation(x, y, c);
+    }
+
+    // ax + by + c = 0
+    // y = -(ax + c) / b
+
+    Point intersectionPoint(LineEquation line2) {
+      log.debug("line1: x={}, y={}, c={}", x, y, c);
+      log.debug("line2: x={}, y={}, c={}", line2.x(), line2.y(), line2.c());
+      double m1 = -x / y;
+      double b1 = -c / y;
+      double m2 = -line2.x / line2.y;
+      double b2 = -line2.c / line2.y;
+      return calculateIntersectionPoint(m1, b1, m2, b2).orElse(Point.of(0, 0));
+    }
+
+    record Point(long x, long y) {
+      static Point of(long x, long y) {
+        return new Point(x, y);
+      }
+    }
+
+    // y = m1x + b1
+    // y = m2x + b2
+    Optional<Point> calculateIntersectionPoint(double m1, double b1, double m2, double b2) {
+
+      if (m1 == m2) {
+        return Optional.empty();
+      }
+
+      double x = (b2 - b1) / (m1 - m2);
+      double y = m1 * x + b1;
+
+      return Optional.of(Point.of(Math.round(x), Math.round(y)));
+    }
+  }
+
+  public record Prize(long x, long y) {
+    public static Prize of(String input) {
+      Pattern pattern = Pattern.compile("Prize: X=(\\d+), Y=(\\d+)");
+      Matcher matcher = pattern.matcher(input);
+      if (matcher.find()) {
+        long x = Long.parseLong(matcher.group(1));
+        long y = Long.parseLong(matcher.group(2));
+        return new Prize(x, y);
+      }
+      return null;
+    }
+  }
+
+  record Button(long x, long y) {
+    public static Button of(String input) {
+      Pattern pattern = Pattern.compile("Button ([AB]): X\\+(\\d+), Y\\+(\\d+)");
+      Matcher matcher = pattern.matcher(input);
+      log.info("input={}", input);
+      log.info("matcher={}", matcher);
+      if (matcher.find()) {
+        String match = matcher.group(1);
+        long x = Long.parseLong(matcher.group(2));
+        long y = Long.parseLong(matcher.group(3));
+        log.info("match={}, x={}, y={}", match, x, y);
+        return new Button(x, y);
+      }
+
+      return null;
+    }
+  }
+}
---
+++ src/test/java/be/bonamis/advent/year2024/Day13Test.java
@@ -0,0 +1,86 @@
+package be.bonamis.advent.year2024;
+
+import static be.bonamis.advent.year2024.Day13.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import be.bonamis.advent.year2024.Day13.LineEquation.Point;
+import org.junit.jupiter.api.*;
+
+class Day13Test {
+
+
+  public static final String INPUT =
+      """
+          Button A: X+94, Y+34
+          Button B: X+22, Y+67
+          Prize: X=8400, Y=5400
+
+          Button A: X+26, Y+66
+          Button B: X+67, Y+21
+          Prize: X=12748, Y=12176
+
+          Button A: X+17, Y+86
+          Button B: X+84, Y+37
+          Prize: X=7870, Y=6450
+
+          Button A: X+69, Y+23
+          Button B: X+27, Y+71
+          Prize: X=18641, Y=10279
+          """;
+
+  @Test
+  void solvePart01() {
+    assertThat(new Day13(INPUT).solvePart01()).isEqualTo(480L);
+  }
+
+  @Test
+  void parseButton() {
+    final String input = "Button A: X+94, Y+34";
+    Button button = Button.of(input);
+    assertThat(button).isNotNull();
+    assertThat(button.x()).isEqualTo(94L);
+    assertThat(button.y()).isEqualTo(34L);
+  }
+
+  @Test
+  void parsePrize() {
+    final String input = "Prize: X=8400, Y=5400";
+    Prize prize = Prize.of(input);
+    assertThat(prize).isNotNull();
+    assertThat(prize.x()).isEqualTo(8400L);
+    assertThat(prize.y()).isEqualTo(5400L);
+  }
+
+  @Test
+  void twoLinesIntersect() {
+    /*
+    y = 3x-3
+    y = 2.3x+4
+    intersection point is (10, 27)
+     */
+    LineEquation line1 = LineEquation.of(3, -1, -3);
+    LineEquation line2 = LineEquation.of(2.3, -1, 4);
+    Point intersectionPoint = line1.intersectionPoint(line2);
+    assertThat(intersectionPoint).isEqualTo(Point.of(10, 27));
+  }
+
+  @Test
+  void twoLinesOfSampleIntersect() {
+    LineEquation line1 = LineEquation.of(94, 22, -8400);
+    LineEquation line2 = LineEquation.of(34, 67, -5400);
+    Point intersectionPoint = line1.intersectionPoint(line2);
+    assertThat(intersectionPoint).isEqualTo(Point.of(80, 40));
+  }
+
+  @Test
+  void inputIntersectionPoint() {
+    Input input = new Day13(INPUT).getInputs().inputs().get(0);
+    assertThat(input.intersectionPoint()).isEqualTo(Point.of(80, 40));
+  }
+
+  @Test
+  void inputHaNonIntersectionPoint() {
+    Input input = new Day13(INPUT).getInputs().inputs().get(1);
+    assertThat(input.intersectionPoint()).isEqualTo(Point.of(0, 0));
+  }
+}
