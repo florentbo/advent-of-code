c0b1d03ad4dee141dc502451b9d4739bcc4ce4d7
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2024-12-21 09:56:49 +0100

    2024 day 19 part 2 setup

--- src/test/java/be/bonamis/advent/year2024/Day19Test.java
+++ src/test/java/be/bonamis/advent/year2024/Day19Test.java
@@ -37,16 +37,6 @@ class Day19Test {
     Day19 day19 = new Day19(new ByteArrayInputStream(input.getBytes()));
     assertThat(day19.getInput().patterns()).hasSize(8);
     assertThat(day19.getInput().designs()).hasSize(8);
-
-    assertThat(day19.canBeMade("brwrr", day19.getInput().patterns())).isTrue();
-    assertThat(day19.canBeMade("bggr", day19.getInput().patterns())).isTrue();
-    assertThat(day19.canBeMade("gbbr", day19.getInput().patterns())).isTrue();
-    assertThat(day19.canBeMade("rrbgbr", day19.getInput().patterns())).isTrue();
-    assertThat(day19.canBeMade("ubwu", day19.getInput().patterns())).isFalse();
-    assertThat(day19.canBeMade("bwurrg", day19.getInput().patterns())).isTrue();
-    assertThat(day19.canBeMade("brgr", day19.getInput().patterns())).isTrue();
-    assertThat(day19.canBeMade("bbrgwb", day19.getInput().patterns())).isFalse();
-
     assertThat(day19.solvePart01()).isEqualTo(6);
   }
 
