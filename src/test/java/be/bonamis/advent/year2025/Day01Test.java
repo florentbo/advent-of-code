2557dbb75759859adce16eabeb1a31e435dff147
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-12-01 06:40:08 +0100

    2025 day 01 part 1 ok

---
+++ src/test/java/be/bonamis/advent/year2025/Day01Test.java
@@ -0,0 +1,44 @@
+package be.bonamis.advent.year2025;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class Day01Test {
+
+  String sampleInput =
+"""
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+""";
+
+  /*@Test
+  void inputLineOf() {
+  	Day01 day01 = new Day01(sampleInput);
+  	Day01.Input input = day01.getInput();
+  	assertThat(input.lines().size()).isEqualTo(10);
+  	assertThat(input.zeroCount()).isEqualTo(3);
+  }*/
+
+  @Test
+  void solvePart01() {
+    Day01 day01 = new Day01(sampleInput);
+    long solvePart01 = day01.solvePart01();
+    assertThat(solvePart01).isEqualTo(3);
+  }
+
+  @Test
+  void solvePart02() {
+    Day01 day01 = new Day01(sampleInput);
+    long solvePart01 = day01.solvePart02();
+    assertThat(solvePart01).isEqualTo(6);
+  }
+}
