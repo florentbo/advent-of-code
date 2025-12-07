302c2378fcc200a1ba6e1fd1ab17b484ec17399c
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-12-07 21:39:47 +0100

    2025 day 05 part 2 ok

--- src/main/java/be/bonamis/advent/year2025/Day05.java
+++ src/main/java/be/bonamis/advent/year2025/Day05.java
@@ -3,7 +3,9 @@ package be.bonamis.advent.year2025;
 import be.bonamis.advent.TextDaySolver;
 import java.io.InputStream;
 import java.util.*;
-import java.util.stream.*;
+
+import be.bonamis.advent.year2025.Day05.Input.FreshIngredientsRange;
+import be.bonamis.advent.year2025.Day05.Input.FreshIngredientsRanges;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,12 +26,21 @@ public class Day05 extends TextDaySolver {
   }
 
   record Input(FreshIngredientsRanges ranges, AvailableIngredients availableIngredient) {
-    record FreshIngredientsRanges(List<FreshIngredientsRange> ranges) {}
+    record FreshIngredientsRanges(List<FreshIngredientsRange> ranges) {
+
+      long totalSize() {
+        return ranges.stream().mapToLong(FreshIngredientsRange::size).sum();
+      }
+    }
 
     record FreshIngredientsRange(long start, long end) {
       static FreshIngredientsRange of(String line) {
         String[] split = line.split("-");
         return new FreshIngredientsRange(Long.parseLong(split[0]), Long.parseLong(split[1]));
+      }
+
+      long size() {
+        return end - start + 1;
       }
 
       boolean isFresh(long ingredient) {
@@ -68,6 +79,35 @@ public class Day05 extends TextDaySolver {
 
   @Override
   public long solvePart02() {
-    return 1000;
+    FreshIngredientsRanges ranges = this.input.ranges();
+    int previousSize;
+
+    do {
+      previousSize = ranges.ranges().size();
+      ranges = mergeRanges(ranges.ranges());
+    } while (ranges.ranges().size() != previousSize);
+    return ranges.totalSize();
+  }
+
+  private FreshIngredientsRanges mergeRanges(List<FreshIngredientsRange> ranges) {
+    List<FreshIngredientsRange> mergedRanges = new ArrayList<>();
+    for (FreshIngredientsRange range : ranges) {
+      boolean merged = false;
+      for (int i = 0; i < mergedRanges.size(); i++) {
+        FreshIngredientsRange mergedRange = mergedRanges.get(i);
+        if (range.start() <= mergedRange.end() + 1 && range.end() >= mergedRange.start() - 1) {
+          long newStart = Math.min(range.start(), mergedRange.start());
+          long newEnd = Math.max(range.end(), mergedRange.end());
+          mergedRanges.set(i, new FreshIngredientsRange(newStart, newEnd));
+          merged = true;
+          break;
+        }
+      }
+      if (!merged) {
+        mergedRanges.add(range);
+      }
+      log.info("merged ranges: {}", mergedRanges);
+    }
+    return new FreshIngredientsRanges(mergedRanges);
   }
 }
--- src/test/java/be/bonamis/advent/year2025/Day05Test.java
+++ src/test/java/be/bonamis/advent/year2025/Day05Test.java
@@ -10,26 +10,27 @@ import be.bonamis.advent.year2025.Day05.Input.FreshIngredientsRanges;
 
 class Day05Test {
 
+  private static final String INPUT =
+      """
+			3-5
+			10-14
+			16-20
+			12-18
+
+			1
+			5
+			8
+			11
+			17
+			32
+			""";
+  private final Day05 day05 = new Day05(INPUT);
+
   @Test
   void solvePart01() {
-    String input =
-        """
-				3-5
-				10-14
-				16-20
-				12-18
-
-				1
-				5
-				8
-				11
-				17
-				32
-				""";
-
-    Day05 day05 = new Day05(input);
     assertThat(day05.solvePart01()).isEqualTo(3);
   }
+
   @Test
   void isFresh() {
     String input =
@@ -44,5 +45,10 @@ class Day05Test {
         .isEqualTo(new FreshIngredientsRanges(of(new FreshIngredientsRange(3, 5))));
     assertThat(day05Input.isFresh(1L)).isFalse();
     assertThat(day05Input.isFresh(3L)).isTrue();
+  }
+
+  @Test
+  void solvePart02() {
+    assertThat(day05.solvePart02()).isEqualTo(14);
   }
 }
